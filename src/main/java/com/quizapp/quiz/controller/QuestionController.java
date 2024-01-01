94266464a329690baf5127e4cd395b663d19f528
Abhijit Mali <maliabhijit183@gmail.com>
2024-01-01 21:23:07 +0530

    feat: getAllQuestion Service Added.

--- src/main/java/com/quizapp/quiz/controller/QuestionController.java
+++ src/main/java/com/quizapp/quiz/controller/QuestionController.java
@@ -1,18 +1,25 @@
 package com.quizapp.quiz.controller;
 
 
+import com.quizapp.quiz.model.Question;
+import com.quizapp.quiz.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/question")
 public class QuestionController {
 
-    @GetMapping("/")
-    public List<String> allQuestions()
+    @Autowired
+    QuestionService questionService;
+    @GetMapping("/allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions()
     {
-        return new List<String>() {
-        }
+        return questionService.getAllQuestions();
     }
 }
---
+++ src/main/java/com/quizapp/quiz/service/QuestionService.java
@@ -0,0 +1,28 @@
+package com.quizapp.quiz.service;
+
+import com.quizapp.quiz.dao.QuestionDao;
+import com.quizapp.quiz.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+}
